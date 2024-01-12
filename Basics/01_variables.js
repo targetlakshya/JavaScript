5346c0350238c943a5be87cf3be6e486628ac210
targetlakshya <thetarget018@gmail.com>
2024-01-12 20:53:15 +0530

    variables complete

---
+++ Basics/01_variables.js
@@ -0,0 +1,21 @@
+console.log("Lakshya Borasi");
+
+const accountID = 144553
+let accountEmail = "Laksyaa@google.com"
+var accountPassword = "136545"
+acountCity = "Bhopal"
+
+// accountID = 2 
+
+accountEmail = "laks@123"
+accountPassword = "498584"
+acountCity = "Indore"
+// console.log(accountID); 
+let accountState ;
+
+console.table([accountID, accountEmail, accountPassword, acountCity,accountState])
+
+/* prefer not to use var
+because of issue in block scope and functional Scope*/
+
+ 
