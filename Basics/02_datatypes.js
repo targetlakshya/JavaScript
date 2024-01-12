6f2584f3ba7ca4a74e4ad3b113cc9e42fb753001
targetlakshya <thetarget018@gmail.com>
2024-01-12 21:06:15 +0530

    DataTypes Done

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"Use Strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node JS not browser
+
+console.log(3+3);
+console.log("Lakshya");
+
+
+let name = "lakshya"
+let age = 18
+let idLoggedIn = false
+
+// Primitive DataTypes
+//  number => 2 to power 53
+//  bigint
+//  string => ""
+//  boolean => true and false
+//  null => standalone value
+//  undefined => abhi value assign nh hui hai
+//  symbol => uniqueness
+
+// Objects
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
