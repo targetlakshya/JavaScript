0023aeeaba186f315d1e212a0b33fe7b1816513b
targetlakshya <thetarget018@gmail.com>
2024-01-19 13:29:09 +0530

    JS Scope Completed

---
+++ 03_Basics/02_Scopes.js
@@ -0,0 +1,19 @@
+// var c = 300
+let a = 300
+
+// let a = 10
+// const b = 20
+// var c = 30
+
+// {} // When the Curly Braces {} are 
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("Inner", a)
+}
+
+
+console.log(a);
+// console.log(b);
+console.log(c); 
